06dbcffe09ab3d6712af3bbadb4c7bc6f56532af
lumirlukas-lgtm <lumirlukas@gmail.com>
2026-03-15 11:54:54 +0100

    Update addon.js

--- addon.js
+++ addon.js
@@ -1,19 +1,44 @@
 const { addonBuilder } = require("stremio-addon-sdk");
 
-const builder = new addonBuilder({
+const manifest = {
   id: "org.muj.helloworldaddon",
   version: "1.0.0",
   name: "Hello World Addon",
   description: "Můj první Stremio addon!",
-  resources: ["catalog"],
+  resources: ["catalog", "meta", "stream"],
   types: ["movie"],
   catalogs: [
-    { type: "movie", id: "helloworldmovies", name: "Hello World filmy" }
+    {
+      type: "movie",
+      id: "helloworldmovies",
+      name: "Hello World filmy",
+      extra: [{ name: "search", isRequired: false }]
+    }
   ]
-});
+};
 
-builder.defineCatalogHandler(function(args) {
-  return Promise.resolve({
+const builder = new addonBuilder(manifest);
+
+
+// CATALOG + SEARCH
+builder.defineCatalogHandler(async function(args) {
+
+  if (args.extra && args.extra.search) {
+    const query = args.extra.search;
+
+    return {
+      metas: [
+        {
+          id: "search-" + query,
+          type: "movie",
+          name: query,
+          poster: "https://via.placeholder.com/300x450"
+        }
+      ]
+    };
+  }
+
+  return {
     metas: [
       {
         id: "tt0032138",
@@ -28,7 +53,34 @@ builder.defineCatalogHandler(function(args) {
         poster: "https://images.metahub.space/poster/medium/tt0017136/img"
       }
     ]
-  });
+  };
+});
+
+
+// META
+builder.defineMetaHandler(async function(args) {
+  return {
+    meta: {
+      id: args.id,
+      type: "movie",
+      name: "Test film",
+      poster: "https://via.placeholder.com/300x450",
+      description: "Test metadata"
+    }
+  };
+});
+
+
+// STREAM
+builder.defineStreamHandler(async function(args) {
+  return {
+    streams: [
+      {
+        title: "Test Stream",
+        url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+      }
+    ]
+  };
 });
 
 module.exports = builder.getInterface();
