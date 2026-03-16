8b608757d72825906d8befdd0a77a3311c7bb0b6
lumirlukas-lgtm <lumirlukas@gmail.com>
2026-03-16 13:55:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,13 +22,20 @@ app.get("/play/:hash/:id", async (req, res) => {
 
   try {
 
-    const page =
-      `https://www.hellspy.to/video/${hash}/${id}`
+   
+      const page =
+  `https://pechal.cz/hellproxy/?url=${encodeURIComponent(`https://www.hellspy.to/video/${hash}/${id}`)}`
 
     const html = await axios.get(page, {
       headers: {
-        "User-Agent": "Mozilla/5.0",
-        "Referer": "https://hellspy.to/"
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://hellspy.to/",
+        "Origin": "https://hellspy.to",
+        "Connection": "keep-alive"
       }
     })
 
